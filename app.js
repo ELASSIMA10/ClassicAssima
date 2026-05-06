06c5c434fd6a3412122ea7a598ad082e84ea0521
Admin <admin@nabil-carte.com>
2026-05-06 21:11:39 +0200

    Fix: aggressive cache busting (v16)

--- app.js
+++ app.js
@@ -126,7 +126,7 @@ function loadTrack(index) {
     }
     
     trackTitle.textContent = track.name; 
-    trackArtist.textContent = "CLASS1C El Assima (v15)";
+    trackArtist.textContent = "CLASS1C El Assima (v16)";
     
     updateMediaSession();
     
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'classic-el-assima-v15';
+const CACHE_NAME = 'classic-el-assima-v16';
 const urlsToCache = [
   './',
   './index.html',
